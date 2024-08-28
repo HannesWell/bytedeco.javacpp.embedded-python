7efd53518a7b621e56d810d4f8bc6976ab1a4406
Hannes Wellmann <hannes.wellmann@iils.de>
2024-08-28 17:41:47 +0200

    More for example

--- src/main/java/MyPythonUse.java
+++ src/main/java/MyPythonUse.java
@@ -1,22 +1,26 @@
+import java.io.IOException;
 import java.util.StringJoiner;
 
+import org.bytedeco.embeddedpython.Pip;
 import org.bytedeco.embeddedpython.Python;
 
 public class MyPythonUse {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
-		StringJoiner lines = new StringJoiner("\n");
-		lines.add("import matplotlib.pyplot as plt");
-		lines.add("import numpy as np");
-		lines.add("x = np.linspace(0, 2 * np.pi, 200)");
-		lines.add("y = np.sin(x)");
-		lines.add("fig, ax = plt.subplots()");
-		lines.add("ax.plot(x, y)");
-		lines.add("fig.savefig('plot.png')");
+        Pip.install("matplotlib", "numpy"); // "numpy"
 
-		Python.eval(lines.toString());
+        StringJoiner lines = new StringJoiner("\n");
+        lines.add("import matplotlib.pyplot as plt");
+        lines.add("import numpy as np");
+        lines.add("x = np.linspace(0, 2 * np.pi, 200)");
+        lines.add("y = np.sin(x)");
+        lines.add("fig, ax = plt.subplots()");
+        lines.add("ax.plot(x, y)");
+        lines.add("fig.savefig('plot.png')");
 
-	}
+        Python.eval(lines.toString());
+
+    }
 
 }
